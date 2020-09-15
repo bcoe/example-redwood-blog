3145d8e09f7d5276629513cffd7bf21525e2b1dc
bcoe <bencoe@google.com>
2020-09-14 23:33:03 -0700

    feat: add auth

--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,6 @@
 import { AuthProvider } from '@redwoodjs/auth'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import netlifyIdentity from 'netlify-identity-widget'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
@@ -9,9 +11,24 @@ import './index.css'
 
 netlifyIdentity.init()
 
+const firebaseClientConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
+
+const firebaseClient = ((config) => {
+  firebase.initializeApp(config)
+  return firebase
+})(firebaseClientConfig)
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <AuthProvider client={netlifyIdentity} type="netlify">
+    <AuthProvider client={firebaseClient} type="firebase">
       <RedwoodProvider>
         <Routes />
       </RedwoodProvider>
